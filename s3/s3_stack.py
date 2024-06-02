93c96b5be6293a09ccbf1191698a259c46a7be71
Andrew Ratcliffe <andrew.ratcliffe@nswcsystems.co.uk>
2024-06-02 20:08:49 +0100

    MyFirstBucket

--- s3/s3_stack.py
+++ s3/s3_stack.py
@@ -1,9 +1,12 @@
 from aws_cdk import (
     # Duration,
     Stack,
+    aws_s3 as s3
     # aws_sqs as sqs,
 )
 from constructs import Construct
+import aws_cdk as cdk
+
 
 class S3Stack(Stack):
 
@@ -17,3 +20,32 @@ class S3Stack(Stack):
         #     self, "S3Queue",
         #     visibility_timeout=Duration.seconds(300),
         # )
+        bucket = s3.Bucket(self, "MyFirstBucket",
+            versioned=True,
+            access_control=s3.BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
+            encryption=s3.BucketEncryption.S3_MANAGED,
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
+            removal_policy=cdk.RemovalPolicy.DESTROY,
+            auto_delete_objects=True,
+            # create a lifecycle rule to move to objects glacier after 1 day
+            lifecycle_rules=[
+                s3.LifecycleRule(
+                    expiration=cdk.Duration.days(180),
+                    transitions=[
+                        s3.Transition(
+                            storage_class=s3.StorageClass.GLACIER,
+                            transition_after=cdk.Duration.days(1),
+                        ),
+                        s3.Transition(
+                            storage_class=s3.StorageClass.DEEP_ARCHIVE,
+                            transition_after=cdk.Duration.days(92),
+                        ),
+                        
+                    ]
+                )
+            ]
+        )
+
+        # grant read/write permissions to the bucket
+        
+
